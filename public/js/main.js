15f79d2eb22c3b6eb51d924fb29a5a983e31d0d0
SKY <SkyKim1016@gmail.com>
2020-01-28 23:14:03 +0900

    Updated Video Play
    Former-commit-id: c704c45d35be9b2299c77e4157da1f38f990ab98

--- public/js/main.js
+++ public/js/main.js
@@ -456,6 +456,26 @@ $(function() {
 
 
 
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+
+
+//Chatting 
 
 hideChat(0);
 
